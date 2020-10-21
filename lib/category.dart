83c80dd5a4d4a1f130b27c473aba35732a39cd69
tizekod <noon.tizekod@gmail.com>
2020-10-21 12:12:06 -0400

    Builds a custom widget called Category

--- lib/category.dart
+++ lib/category.dart
@@ -6,17 +6,34 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+// We use an underscore to indicate that these variables are private.
+// See https://www.dartlang.org/guides/language/effective-dart/design#libraries
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
 /// a colored [InkWell] animation.
 class Category extends StatelessWidget {
+  final String name;
+  final ColorSwatch color;
+  final IconData iconLocation;
+
   /// Creates a [Category].
   ///
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
   // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category(name, color, iconLocation);
+  const Category({
+    Key key,
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        super(key: key);
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -28,6 +45,43 @@ class Category extends StatelessWidget {
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+            borderRadius: _borderRadius,
+            highlightColor: color,
+            splashColor: color,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                // There are two ways to denote a list: `[]` and `List()`.
+                // Prefer to use the literal syntax, i.e. `[]`, instead of `List()`.
+                // You can add the type argument if you'd like, i.e. <Widget>[].
+                // See https://www.dartlang.org/guides/language/effective-dart/usage#do-use-collection-literals-when-possible
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Icon(
+                        iconLocation,
+                        size: 60.0,
+                      )),
+                  Center(
+                    child: Text(
+                      name,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headline,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            onTap: () {
+              print('Inkwell tapped!');
+            }),
+      ),
+    );
   }
 }
